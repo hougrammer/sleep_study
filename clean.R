eb7b5b731d0ba860e4d3c6d001b2ce0f98cad4b8
hougrammer <david.m.hou@gmail.com>
2019-04-14 18:47:01 -0700

    plots

--- clean.R
+++ clean.R
@@ -2,13 +2,16 @@
 library(data.table)
 library(magrittr)
 
-df = data.table(read.csv('data/consolidated.csv'))
+df = data.table(read.csv('data/consolidated.csv', stringsAsFactors = F))
 resp_final = data.table(read.csv('data/EMAIL_Responses-1.csv'))
 resp_init = data.table(read.csv('responses_cleaned.csv'))
 
 df = df %>%
     # get emails
     .[, email := sapply(id, function(x) resp_final[ID == x, Email.Address])] %>%
+    
+    # indicator for whether subject received screen time first 
+    .[, screen_first := sapply(id, function(x) resp_final[ID == x, Week1...screen_time])] %>%
     
     # drop 'S' in id
     .[, id := sapply(id, function(x) substr(as.character(x), 2, 4))] %>%
@@ -19,6 +22,18 @@ df = df %>%
         as.numeric(s[[1]]) + as.numeric(s[[2]])/60
     })] %>%
     
+    # convert start and end to dates
+    # need to do this because some lines have times and some don't
+    # by the way, for 
+    .[, start_date := as.Date(
+        sapply(start, function(x) unlist(strsplit(as.character(x), ' '))[1]),
+        format = '%m/%d/%Y'
+    )] %>%
+    .[, end_date := as.Date(
+        sapply(end, function(x) unlist(strsplit(as.character(x), ' '))[1]),
+        format = '%m/%d/%Y'
+    )] %>%
+    
     # drop hours less than 1
     .[hours > 1, ] %>%
     
@@ -28,7 +43,11 @@ df = df %>%
 # join with cleaned initial responses
 df = df[resp_init, on = 'email']
 
+# write out csv with all subjects (even those who did not participate)
+write.csv(df, 'data/all_subjects.csv', row.names = F)
+
 # remove NAs from people who did not particpate
 df = df[!is.na(id), ]
 
+# write out cleaned csv
 write.csv(df, 'data/cleaned.csv', row.names = F)
